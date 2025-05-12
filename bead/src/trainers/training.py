f957b8d143dcd826909135ec2149f2a7b737e76e
Pratik Jawahar <55033105+PRAkTIKal24@users.noreply.github.com>
2025-05-12 18:31:14 +0100

    partial migration to DDP.

--- bead/src/trainers/training.py
+++ bead/src/trainers/training.py
@@ -19,7 +19,10 @@ import warnings
 
 import numpy as np
 import torch
+import torch.distributed as dist
+from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.data import DataLoader
+from torch.utils.data.distributed import DistributedSampler
 from tqdm import TqdmExperimentalWarning
 from tqdm.rich import tqdm
 
@@ -150,11 +153,12 @@ def train(
     config,
     verbose: bool = False,
 ):
-    """Does the entire training loop by calling the `fit()` and `validate()`. Appart from this, this is the main function where the data is converted
-        to the correct type for it to be trained, via `torch.Tensor()`. Furthermore, the batching is also done here, based on `config.batch_size`,
-        and it is the `torch.utils.data.DataLoader` doing the splitting.
-        Applying either `EarlyStopping` or `LR Scheduler` is also done here, all based on their respective `config` arguments.
-        For reproducibility, the seeds can also be fixed in this function.
+    """
+    Processes the entire training loop by calling the `fit()` and `validate()`. Appart from this, this is the main function where the data is converted
+    to the correct type for it to be trained, via `torch.Tensor()`. Furthermore, the batching is also done here, based on `config.batch_size`,
+    and it is the `torch.utils.data.DataLoader` doing the splitting.
+    Torch AMP and DDP are also implemented here, if the user has selected them in the config file. Applying either `EarlyStopping` or `LR Scheduler` is also done here, all based on their respective `config` arguments.
+    For reproducibility, the seeds can also be fixed in this function using the deterministic_algorithm `config` flag.
 
     Args:
         model (modelObject): The model you wish to train
@@ -162,13 +166,19 @@ def train(
         labels (Tuple): Tuple containing the training and validation labels
         project_path (string): Path to the project directory
         config (dataClass): Base class selecting user inputs
+        verbose (bool): If True, prints additional information during training
 
     Returns:
         modelObject: fully trained model ready to perform compression and decompression
     """
 
+    # Get DDP parameters from config
+    is_ddp_active = config.is_ddp_active
+    local_rank = config.local_rank
+    world_size = config.world_size
+
     # Get the device and move tensors to the device
-    device = helper.get_device()
+    device = helper.get_device(config)
 
     # Reshape tensors to pass to conv layers
     if "ConvVAE" in config.model_name or "ConvAE" in config.model_name:
@@ -249,9 +259,24 @@ def train(
         print(f"Model architecture:\n{model}")
 
     model = model.to(device)
+
+    if is_ddp_active:
+        # find_unused_parameters can be True if parts of the model aren't used in forward pass
+        # which can happen with complex models or conditional execution paths.
+        model = DDP(
+            model,
+            device_ids=[local_rank],
+            output_device=local_rank,
+            find_unused_parameters=False,
+        )
+        if verbose:
+            print(
+                f"Distributed Data Parallel (DDP) initialized. Running on {world_size} GPUs."
+            )
+
     if verbose:
         print(f"Device used for training: {device}")
-        print("Inputs and model moved to device")
+        print("Model moved to device")
 
     # Pushing input data into the torch-DataLoader object and combines into one DataLoader object (a basic wrapper
     # around several DataLoader objects).
@@ -260,85 +285,67 @@ def train(
             "Loading data into DataLoader and using batch size of ", config.batch_size
         )
 
-    if config.deterministic_algorithm:
-        if config.verbose:
-            print("Deterministic algorithm is set to True")
-        torch.backends.cudnn.deterministic = True
-        random.seed(0)
-        torch.manual_seed(0)
-        np.random.seed(0)
-        torch.use_deterministic_algorithms(True)
-        g = torch.Generator()
-        g.manual_seed(0)
+    # DataLoaders with DistributedSampler
+    train_dataset_map = {
+        "event": ds["events_train"],
+        "jet": ds["jets_train"],
+        "constituent": ds["constituents_train"],
+    }
+    val_dataset_map = {
+        "event": ds["events_val"],
+        "jet": ds["jets_val"],
+        "constituent": ds["constituents_val"],
+    }
 
-        train_dl_list = [
-            DataLoader(
-                ds,
-                batch_size=config.batch_size,
-                shuffle=False,
-                worker_init_fn=seed_worker,
-                generator=g,
-                drop_last=True,
-                num_workers=config.parallel_workers,
-                pin_memory=True,
-            )
-            for ds in [ds["events_train"], ds["jets_train"], ds["constituents_train"]]
-        ]
-        valid_dl_list = [
-            DataLoader(
-                ds,
-                batch_size=config.batch_size,
-                shuffle=False,
-                worker_init_fn=seed_worker,
-                generator=g,
-                drop_last=True,
-                num_workers=config.parallel_workers,
-                pin_memory=True,
-            )
-            for ds in [ds["events_val"], ds["jets_val"], ds["constituents_val"]]
-        ]
-    else:
-        train_dl_list = [
-            DataLoader(
-                ds,
-                batch_size=config.batch_size,
-                shuffle=False,
-                drop_last=True,
-                num_workers=config.parallel_workers,
-                pin_memory=True,
-            )
-            for ds in [ds["events_train"], ds["jets_train"], ds["constituents_train"]]
-        ]
-        valid_dl_list = [
-            DataLoader(
-                ds,
-                batch_size=config.batch_size,
-                shuffle=False,
-                drop_last=True,
-                num_workers=config.parallel_workers,
-                pin_memory=True,
-            )
-            for ds in [ds["events_val"], ds["jets_val"], ds["constituents_val"]]
-        ]
-    # Unpacking the DataLoader lists
-    train_dl_events, train_dl_jets, train_dl_constituents = train_dl_list
-    val_dl_events, val_dl_jets, val_dl_constituents = valid_dl_list
+    train_ds_selected = train_dataset_map[config.input_level]
+    val_ds_selected = val_dataset_map[config.input_level]
 
-    if config.model_name == "pj_ensemble":
-        if verbose:
-            print("Model is an ensemble model")
-    else:
-        if config.input_level == "event":
-            train_dl = train_dl_events
-            valid_dl = val_dl_events
-        elif config.input_level == "jet":
-            train_dl = train_dl_jets
-            valid_dl = val_dl_jets
-        elif config.input_level == "constituent":
-            train_dl = train_dl_constituents
-            valid_dl = val_dl_constituents
-        if verbose:
-            print(f"Input data is of {config.input_level} level")
+    if verbose:
+        print(f"Input data is of {config.input_level} level")
+
+    train_sampler = None
+    val_sampler = None
+    shuffle_train = not is_ddp_active  # sampler handles it for DDP
+
+    if is_ddp_active:
+        train_sampler = DistributedSampler(
+            train_ds_selected,
+            num_replicas=world_size,
+            rank=local_rank,
+            shuffle=True,
+            drop_last=True,
+        )
+        val_sampler = DistributedSampler(
+            val_ds_selected,
+            num_replicas=world_size,
+            rank=local_rank,
+            shuffle=False,
+            drop_last=True,
+        )
+
+    common_loader_args = {
+        "batch_size": config.batch_size,
+        "drop_last": True,  # Important for DDP to have same number of batches per GPU
+        "num_workers": config.parallel_workers,
+        "pin_memory": True,
+        "worker_init_fn": seed_worker if config.deterministic_algorithm else None,
+        "generator": torch.Generator().manual_seed(0)
+        if config.deterministic_algorithm
+        else None,
+    }
+
+    train_dl = DataLoader(
+        train_ds_selected,
+        sampler=train_sampler,
+        shuffle=shuffle_train,
+        **common_loader_args,
+    )
+    valid_dl = DataLoader(
+        val_ds_selected,
+        sampler=val_sampler,
+        shuffle=False,
+        **common_loader_args,
+    )
 
     # Select Loss Function
     try:
@@ -358,6 +365,11 @@ def train(
             print(f"Optimizer: {config.optimizer}")
     except ValueError as e:
         print(e)
+
+    # AMP GradScaler
+    amp_scaler = torch.cuda.amp.GradScaler(
+        enabled=(config.use_amp and torch.cuda.is_available())
+    )
 
     # Activate early stopping
     if config.early_stopping:
@@ -389,15 +401,20 @@ def train(
     start = time.time()
 
     # Registering hooks for activation extraction
-    if config.activation_extraction:
+    if config.activation_extraction and not is_ddp_active:
         hooks = model.store_hooks()
 
     if verbose:
         print(f"Beginning training for {config.epochs} epochs")
 
     for epoch in range(config.epochs):
-        print(f"Epoch {epoch + 1} / {config.epochs}")
+        if is_ddp_active and train_sampler is not None:
+            train_sampler.set_epoch(epoch)
 
+        if not is_ddp_active or local_rank == 0:  # Only rank 0 prints epoch info
+            print(f"Epoch {epoch + 1} / {config.epochs}")
+
+        # Call fit (ensure fit is adapted for AMP and DDP verbosity)
         train_losses, train_epoch_loss, model = fit(
             config=config,
             model=model,
@@ -406,46 +423,95 @@ def train(
             reg_param=config.reg_param,
             optimizer=optimizer,
             device=device,
+            scaler=amp_scaler,
+            is_ddp_active=is_ddp_active,
+            local_rank=local_rank,
         )
-        train_loss.append(train_epoch_loss.detach().cpu().numpy())
-        train_loss_data.append(train_losses)
 
-        if 1 - config.train_size:
-            val_losses, val_epoch_loss = validate(
-                config=config,
-                model=model,
-                dataloader=valid_dl,
-                loss_fn=loss_fn,
-                reg_param=config.reg_param,
-                device=device,
-            )
-            val_loss.append(val_epoch_loss.detach().cpu().numpy())
-            val_loss_data.append(val_losses)
-        else:
-            val_epoch_loss = train_epoch_loss
-            val_losses = train_losses
-            val_loss.append(val_epoch_loss)
-            val_loss_data.append(val_losses)
+        # For simplicity, we'll let rank 0 do most of the logging and state updates like early stopping.
+        if not is_ddp_active or local_rank == 0:
+            train_loss.append(
+                train_epoch_loss.detach().cpu().numpy()
+            )  # Ensure this is from rank 0 or averaged
+            train_loss_data.append(
+                train_losses
+            )  # This might need care with DDP if losses are tensors
 
-        # Implementing LR Scheduler
-        if config.lr_scheduler:
-            lr_scheduler(val_epoch_loss)
+            if 1 - config.train_size > 0:  # If there's a validation set
+                val_losses, val_epoch_loss = validate(
+                    config=config,
+                    model=model,  # Pass the DDP model if active
+                    dataloader=valid_dl,
+                    loss_fn=loss_fn,
+                    reg_param=config.reg_param,
+                    device=device,
+                    is_ddp_active=is_ddp_active,
+                    local_rank=local_rank,
+                )
+                val_loss.append(val_epoch_loss.detach().cpu().numpy())
+                val_loss_data.append(val_losses)
+            else:  # No validation set, use training loss for schedulers/stopping
+                val_epoch_loss = train_epoch_loss
+                val_loss.append(val_epoch_loss.detach().cpu().numpy())
+                val_loss_data.append(train_losses)
 
-        ## Implementation to save models & values after every N config.epochs, where N is stored in 'config.intermittent_saving_patience':
-        if config.intermittent_model_saving:
-            if epoch % config.intermittent_saving_patience == 0:
-                path = os.path.join(output_path, "models", f"model_{epoch}.pt")
-                helper.save_model(model, path)
+            if config.lr_scheduler:
+                lr_scheduler(val_epoch_loss)
 
-        # Implementing Early Stopping
-        if config.early_stopping:
-            early_stopper(val_epoch_loss)
-            if early_stopper.early_stop:
-                if verbose:
-                    print("Early stopping activated at epoch ", epoch)
-                break
+            if config.intermittent_model_saving:
+                if epoch % config.intermittent_saving_patience == 0:
+                    path = os.path.join(
+                        output_path, "models", f"model_epoch_{epoch}.pt"
+                    )
+                    helper.save_model(
+                        model, path, config
+                    )  # Pass config for DDP awareness
+
+            if config.early_stopping:
+                early_stopper(val_epoch_loss)
+                if early_stopper.early_stop:
+                    if verbose:
+                        print(
+                            f"Rank {local_rank}: Early stopping activated at epoch {epoch}"
+                        )
+                    # Need to signal other processes to stop if DDP is active
+                    if is_ddp_active:
+                        stop_signal = torch.tensor([1.0], device=device)
+                    break  # Rank 0 breaks
+        # DDP: check for stop signal from rank 0
+        if is_ddp_active:
+            if "stop_signal" not in locals():  # Initialize if not set by rank 0
+                stop_signal = torch.tensor([0.0], device=device)
+            dist.broadcast(stop_signal, src=0)  # Rank 0 broadcasts its stop_signal
+            if stop_signal.item() == 1.0:
+                if verbose and local_rank != 0:
+                    print(
+                        f"Rank {local_rank}: Received early stop signal at epoch {epoch}."
+                    )
+                break  # Other ranks break
 
     end = time.time()
+
+    if not is_ddp_active or local_rank == 0:
+        helper.save_model(
+            model, os.path.join(output_path, "models", "model.pt"), config
+        )
+        # ... save other results like loss arrays ...
+        np.save(
+            os.path.join(save_dir, "train_epoch_loss_data.npy"), np.array(train_loss)
+        )
+        np.save(os.path.join(save_dir, "val_epoch_loss_data.npy"), np.array(val_loss))
+        # ... (saving mu_data, etc. - if these are collected per epoch, ensure DDP handling)
+
+    # If activations are extracted, this needs to be DDP aware or run post-training on a single GPU
+    if config.activation_extraction and (not is_ddp_active or local_rank == 0):
+        # model.module if DDP, else model
+        actual_model = model.module if is_ddp_active else model
+        # hooks = actual_model.store_hooks() # Assuming store_hooks is on base model
+        # ... run a pass to get activations ...
+        # actual_model.detach_hooks(hooks)
+
+    return model  # Return the DDP model or base model
 
     # Run a final forward pass on training data to get final latent space representation
     # Output Lists
