9cc3b2175feafeb957cc387cb979ac9f2d095734
Pratik Jawahar <55033105+PRAkTIKal24@users.noreply.github.com>
2025-05-13 18:12:33 +0100

    updating ddp wrapper to find unused ldj for non-flow models

--- bead/src/trainers/training.py
+++ bead/src/trainers/training.py
@@ -307,7 +307,7 @@ def train(
             model,
             device_ids=[local_rank],
             output_device=local_rank,
-            find_unused_parameters=False,
+            find_unused_parameters=True,
         )
         if verbose and (not is_ddp_active or local_rank == 0):
             print(
