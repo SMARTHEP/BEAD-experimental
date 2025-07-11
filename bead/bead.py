b3be6243d1063d05cc8d53137807a24e950c17d7
Pratik Jawahar <55033105+PRAkTIKal24@users.noreply.github.com>
2025-07-11 15:12:39 +0100

    minor bug fix

--- bead/bead.py
+++ bead/bead.py
@@ -127,6 +127,8 @@ def main():
         ),
     }
 
+    config.skip_to_roc = False  # Default value for skip_to_roc
+
     # Check what the options flag is set to and override the default if necessary
     if options == "h5" or options == "npy":
         config.file_type = options
